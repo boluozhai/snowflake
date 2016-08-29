06884436c57d8f00698afe962528dd2b113e0a19
xukun <xukun17@sina.com>
2016-08-29 16:18:43 +0800

    implements : js-lib 'listview.js'.

---
+++ snowflake8/projects/snowflake-lib-webapp-resources/src/main/webapp/test/test-listview.js
@@ -0,0 +1,14 @@
+JS.module(function(mc) {
+
+	mc.package('test');
+
+	function TestListView() {
+	}
+
+	mc.class(function(cc) {
+		cc.type(TestListView);
+	});
+
+	TestListView.prototype = {};
+
+});
